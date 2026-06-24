aea55587951f016b5695a9de3c76c09626d4bfbf
nicole <nicole732470@gmail.com>
2026-06-23 19:48:03 -0500

    Find user-local SSM tunnel plugin

--- deploy/open_dashboard_tunnel.sh
+++ deploy/open_dashboard_tunnel.sh
@@ -5,6 +5,10 @@ REGION="${REGION:-us-east-2}"
 EC2_INSTANCE="${EC2_INSTANCE:-i-0bdee6f611283586f}"
 LOCAL_PORT="${LOCAL_PORT:-8501}"
 
+if [[ -x "$HOME/.local/bin/session-manager-plugin" ]]; then
+  export PATH="$HOME/.local/bin:$PATH"
+fi
+
 echo "Keep this terminal open, then visit http://127.0.0.1:${LOCAL_PORT}"
 exec aws ssm start-session \
   --region "$REGION" \
