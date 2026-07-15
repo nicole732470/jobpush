9ed72b0f57f2a283571cf0e616f81718072d472a
nicole <nicole732470@gmail.com>
2026-07-14 21:02:52 -0500

    Use bounded sponsorship exclusion phrases

--- db/migrations/182_unify_platform_and_export_eligibility.sql
+++ db/migrations/182_unify_platform_and_export_eligibility.sql
@@ -2,7 +2,18 @@ BEGIN;
 
 CREATE OR REPLACE FUNCTION jobpush.is_explicit_no_sponsorship(description TEXT)
 RETURNS BOOLEAN LANGUAGE sql IMMUTABLE AS $$
-  SELECT COALESCE(description, '') ~* '((unable|cannot|can not|will not|do not|does not|not able to|no longer).{0,80}(sponsor|sponsorship)|(no|without).{0,50}(visa|employment|work authorization).{0,50}sponsorship|(visa|employment|work authorization).{0,50}sponsorship.{0,40}(not available|unavailable|not provided)|must.{0,80}(authorized|eligible).{0,100}without.{0,50}sponsorship|not.{0,30}eligible.{0,50}(visa )?sponsorship|sponsorship.{0,40}(is )?not (available|offered|provided))'
+  SELECT lower(COALESCE(description, '')) LIKE ANY (ARRAY[
+    '%unable to sponsor%', '%unable to provide sponsorship%',
+    '%cannot sponsor%', '%can not sponsor%', '%will not sponsor%',
+    '%do not sponsor%', '%does not sponsor%', '%not able to sponsor%',
+    '%no longer sponsor%', '%no sponsorship%', '%without sponsorship%',
+    '%without visa sponsorship%', '%without employment sponsorship%',
+    '%without work authorization sponsorship%',
+    '%sponsorship is not available%', '%sponsorship not available%',
+    '%sponsorship is not offered%', '%sponsorship not offered%',
+    '%sponsorship is not provided%', '%sponsorship not provided%',
+    '%visa sponsorship is unavailable%', '%visa sponsorship unavailable%'
+  ])
 $$;
 
 COMMIT;
