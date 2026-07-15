b225f17c9b43a052193189867d272efcaa545ac3
nicole <nicole732470@gmail.com>
2026-07-14 22:51:26 -0500

    Run JobScore sites in due crawl batches

--- db/run_due_crawl_batch.sh
+++ db/run_due_crawl_batch.sh
@@ -78,6 +78,8 @@ for row in "${DUE_SITES[@]}"; do
       adapter_name="workable-markdown"; adapter_version="0.1.0"; adapter_script="scripts/crawl_workable.py" ;;
     jobvite)
       adapter_name="jobvite-html-jsonld"; adapter_version="0.1.0"; adapter_script="scripts/crawl_jobvite.py" ;;
+    jobscore)
+      adapter_name="jobscore-html"; adapter_version="0.1.0"; adapter_script="scripts/crawl_jobscore.py" ;;
     applicantpro)
       adapter_name="applicantpro-api"; adapter_version="0.1.0"; adapter_script="scripts/crawl_applicantpro.py" ;;
     dover)
